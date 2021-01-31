092774d86796567f4869f69c05fc7486974d415f
ajeetchaulagain <chaulagainajeet@gmail.com>
2021-01-31 17:06:07 +1100

    tailwind configured

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import "../src/styles/globalStyles.css";
+
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default App;
--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const IndexPage = () => {
   return (
-    <div>
+    <div className="bg-blue-500 text-white px-10 py-10">
       <h1>Welcome to Mudbath!</h1>
     </div>
   );
