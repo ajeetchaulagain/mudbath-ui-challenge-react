534eedfc4d819792f430c82030d8d8a447e03a01
ajeetchaulagain <chaulagainajeet@gmail.com>
2021-02-08 11:12:43 +1100

    add test for index page

--- src/tests/pages/index.test.js
+++ src/tests/pages/index.test.js
@@ -1,5 +1,28 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import "next/router";
+
+import IndexPage from "../../../pages/index";
+
+//Mocking nextjs router
+jest.mock("next/router", () => ({
+  useRouter() {
+    return {
+      route: "/",
+      pathname: "",
+      query: "",
+      asPath: "",
+      push: jest.fn(),
+    };
+  },
+}));
+
 describe("<IndexPage/> Component", () => {
-  it("dummy test", () => {
-    expect(1).toBe(1);
+  it("should render a index page with button", () => {
+    const { getByText } = render(<IndexPage />);
+    const viewButton = getByText(/view/i);
+    expect(viewButton).toBeInTheDocument();
+    fireEvent.click(viewButton);
   });
 });
